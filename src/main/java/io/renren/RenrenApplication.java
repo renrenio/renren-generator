b983c4c8c0cf8ebf01f2e2cd5dbc519f1ea7b388
gxz <514190950@qq.com>
2020-05-12 14:52:27 +0800

    解决了使用非Mongo加载相应依赖的问题 同时优化了内部类的创建命名

--- src/main/java/io/renren/RenrenApplication.java
+++ src/main/java/io/renren/RenrenApplication.java
@@ -3,8 +3,10 @@ package io.renren;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 @MapperScan("io.renren.dao")
 public class RenrenApplication {
 
